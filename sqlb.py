45435ff641aa372fcc479948818ca6d30756200a
Gilberto Gradias <ggradias@bitbucket.org>
2016-11-03 23:00:39 -0700

    ejercicios sqlite3

--- sqlb.py
+++ sqlb.py
@@ -4,7 +4,14 @@ conn = sqlite3.connect("new.db")
 
 cursor = conn.cursor()
 
-cursor.execute("""CREATE TABLE population (city TEXT, state TEXT, population INT) """)
+#cursor.execute("""CREATE TABLE population (city TEXT, state TEXT, population INT) """)
+
+
+cursor.execute("INSERT INTO population VALUES ('New York City','NY',8200000)")
+cursor.execute("INSERT INTO population VALUES ('San Francisco','CA',8000000)")
+
+# commit the changes
+conn.commit()
 
 conn.close()
 
